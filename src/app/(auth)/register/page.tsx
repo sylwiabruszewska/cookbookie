03487b05f6b1702398a2d505ee0964a093f11a44
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-03-03 21:12:21 +0100

    update register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,9 @@
+import RegistrationForm from "@/app/ui/components/auth/register-form";
+
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <p>Register Page</p>
+      <RegistrationForm />
     </div>
   );
 }

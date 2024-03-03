4c704662a719ab09e0878a40aeeb520b75c7e496
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-03-03 15:20:52 +0100

    add gradient for bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { poppins } from "@/app/ui/fonts";
 export default function Home() {
   return (
     <main className="relative flex min-h-screen min-w-screen flex-col justify-center main-background bg-center bg-cover">
-      <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-50 z-0"></div>
 
       <div className="z-10 text-white flex flex-col items-center gap-4">
         <Image

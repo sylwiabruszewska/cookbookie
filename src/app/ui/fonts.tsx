c46dffd63de9e08e67b32c913edaba7770b161d9
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-03-03 15:11:35 +0100

    update homepage, fonts

--- src/app/ui/fonts.tsx
+++ src/app/ui/fonts.tsx
@@ -1,3 +1,8 @@
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+
+export const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});

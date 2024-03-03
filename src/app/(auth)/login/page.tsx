6ce781a8234ba087feb4d261e41e0dda3654445d
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-03-03 21:15:05 +0100

    update login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,9 @@
+import LoginForm from "@/app/ui/components/auth/login-form";
+
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <p>Login Page</p>
+      <LoginForm />
     </div>
   );
 }

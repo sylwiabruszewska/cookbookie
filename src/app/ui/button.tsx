ccfe41dd3a51650553a68b0e28978cc90ceb721e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-03-03 21:16:31 +0100

    update button with onclick

--- src/app/ui/button.tsx
+++ src/app/ui/button.tsx
@@ -2,12 +2,14 @@ import clsx from "clsx";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  onClick?: () => void;
 }
 
-export function Button({ children, className, ...rest }: ButtonProps) {
+export function Button({ children, className, onClick, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
+      onClick={onClick}
       className={clsx(
         "flex h-10 items-center justify-center rounded-lg bg-orange-500 px-6 text-sm font-medium text-white transition-colors hover:bg-orange-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500 active:bg-orange-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
         className

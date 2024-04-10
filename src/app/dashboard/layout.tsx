ea93c1a5c80e928619e8e4202b62e893a60cf6fe
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-10 15:55:28 +0200

    add logo, nav in header

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <>
         <header className=" text-gray-800 py-4 w-full">
-          <div className="container mx-auto">
-            <p className="text-xl">Header</p>
+          <div className="container mx-auto flex items-center justify-between">
+            <Link href="/">
+              <Image
+                src="/logo.svg"
+                alt="CookBookie Logo"
+                className="dark:invert"
+                width={44}
+                height={44}
+                priority
+              />
+            </Link>
+
+            <nav className="flex flex-grow justify-center">
+              <ul className="flex space-x-4">
+                <li>
+                  <Link href="/add-recipe">Add recipes</Link>
+                </li>
+                <li>
+                  <Link href="/my-recipes">My recipes</Link>
+                </li>
+                <li>
+                  <Link href="/favorites">Favorites</Link>
+                </li>
+                <li>
+                  <Link href="/shopping-list">Shopping list</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="flex items-center">
+              <div className="ml-4">Search</div>
+              <div className="ml-4">User</div>
+            </div>
           </div>
         </header>
 

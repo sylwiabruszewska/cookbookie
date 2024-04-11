90208126e7ad833669af20f389a369006c2c9c58
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-11 15:13:32 +0200

    add blob fog hero

--- src/app/ui/components/dashboard/hero/hero.tsx
+++ src/app/ui/components/dashboard/hero/hero.tsx
@@ -3,6 +3,20 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <div className="flex justify-center items-center lg:h-[40vh]">
+      <div
+        style={{
+          backgroundImage: "url(/blob.svg)",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "500px",
+          height: "500px",
+          position: "absolute",
+          top: "0",
+          right: "0",
+          zIndex: "-100",
+        }}
+      ></div>
+
       <div className="text-center ">
         <h1 className="text-4xl font-bold">CookBookie</h1>
       </div>

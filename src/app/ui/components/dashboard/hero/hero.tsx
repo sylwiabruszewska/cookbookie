cc08b6c0e832a87effef79f6bc7233f80e8b5c57
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-11 15:13:18 +0200

    create hero component

---
+++ src/app/ui/components/dashboard/hero/hero.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <div className="flex justify-center items-center lg:h-[40vh]">
+      <div className="text-center ">
+        <h1 className="text-4xl font-bold">CookBookie</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;

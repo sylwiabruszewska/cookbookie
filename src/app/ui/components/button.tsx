beae7f5861d9fa7ace7f463365db48b70c867715
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-15 15:16:44 +0200

    update button colors

--- src/app/ui/components/button.tsx
+++ src/app/ui/components/button.tsx
@@ -11,7 +11,7 @@ export function Button({ children, className, onClick, ...rest }: ButtonProps) {
       {...rest}
       onClick={onClick}
       className={clsx(
-        "flex h-10 items-center justify-center rounded-lg bg-orange-500 px-6 text-sm font-medium text-white transition-colors hover:bg-orange-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500 active:bg-orange-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
+        "flex h-10 items-center justify-center rounded-lg bg-[--primary-color] px-6 text-sm font-medium text-white transition-colors hover:bg-transparent border-2 border-[--primary-color] hover:border-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[--primary-color] active:bg-[--primary-color] aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
         className
       )}
     >

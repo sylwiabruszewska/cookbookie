c1c7cc776aee50a3eddc7e318b948883af0e08ad
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-15 14:46:51 +0200

    update search form styles

--- src/app/ui/components/dashboard/search-form.tsx
+++ src/app/ui/components/dashboard/search-form.tsx
@@ -10,21 +10,37 @@ const SearchForm: React.FC = () => {
       onSubmit={handleSubmit}
       className="flex items-center justify-center mt-8"
     >
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        type="text"
-        id="search"
-        placeholder="Search recipes"
-        className="border border-slate-500 rounded-l px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[--primary-color]"
-      />
-      <button
-        type="submit"
-        className="bg-slate-500 hover:bg-[--primary-color] text-white rounded-r px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      >
-        Search
-      </button>
+      <div className="relative">
+        <label htmlFor="search" className="sr-only">
+          Search
+        </label>
+        <input
+          type="text"
+          id="search"
+          placeholder="Search recipes"
+          className="block border border-slate-500 rounded-l rounded-r px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[--primary-color] focus:border-[--primary-color]"
+        />
+        <button
+          type="submit"
+          className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full bg-slate-500 hover:bg-[--primary-color] text-white rounded-r px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[--primary-color]"
+        >
+          <svg
+            className="w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </button>
+      </div>
     </form>
   );
 };

4c184074878dd02d4702a90a0ce4fc1be9987546
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-16 13:41:37 +0200

    create recipe card component

---
+++ src/app/ui/components/recipe-card.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function RecipeCard() {
+  return (
+    <li className="mb-8">
+      <Link href="/">
+        <div className="relative h-323 rounded-lg">
+          <Image
+            src="/pancakes.png"
+            width={1000}
+            height={323}
+            className="object-cover"
+            alt="Description"
+          />
+          <div className="absolute bottom-0 left-0 right-0 m-4 bg-white rounded-lg p-4">
+            Description
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
+}

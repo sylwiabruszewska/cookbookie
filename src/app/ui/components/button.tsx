f435c1e680cc0013e844d50fbc59646abd55c319
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-16 13:54:47 +0200

    add btn secondary variant

--- src/app/ui/components/button.tsx
+++ src/app/ui/components/button.tsx
@@ -3,15 +3,28 @@ import clsx from "clsx";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   onClick?: () => void;
+  variant?: "primary" | "secondary";
 }
 
-export function Button({ children, className, onClick, ...rest }: ButtonProps) {
+export function Button({
+  children,
+  className,
+  onClick,
+  variant = "primary",
+  ...rest
+}: ButtonProps) {
   return (
     <button
       {...rest}
       onClick={onClick}
       className={clsx(
-        "flex h-10 items-center justify-center rounded-lg bg-[--primary-color] px-6 text-sm font-medium text-white transition-colors hover:bg-transparent border-2 border-[--primary-color] hover:border-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[--primary-color] active:bg-[--primary-color] aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
+        "flex h-10 items-center justify-center rounded-lg px-6 text-sm font-medium transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 active:bg-[--primary-color] aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
+        {
+          "bg-[--primary-color] text-white border-2 border-[--primary-color] hover:bg-transparent hover:border-white":
+            variant === "primary",
+          "bg-[--primary-color] text-white hover:bg-[--gray-dark] hover:text-white":
+            variant === "secondary",
+        },
         className
       )}
     >

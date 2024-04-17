75b3b2c520fe2eddb1a40d8f16b3221b70f9d719
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-17 15:11:59 +0200

    move hero section to components

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,9 +1,17 @@
-import HeroSection from "@/app/ui/components/dashboard/hero/hero";
+"use client";
+
+import HeroSection from "@/app/ui/components/dashboard/hero";
+import SearchForm from "@/app/ui/components/dashboard/search-form";
+import CategoryCard from "@/app/ui/components/category-card";
 
 export default function Page() {
   return (
     <>
       <HeroSection />
+      <SearchForm />
+      <ul className="list-none">
+        <CategoryCard />
+      </ul>
     </>
   );
 }

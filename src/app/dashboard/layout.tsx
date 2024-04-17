6e281a2b471a9dbb66721dde018addd4cbe81959
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-17 15:27:17 +0200

    update footer styles

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -73,49 +73,61 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
         </main>
 
-        <footer className="bg-[--gray-dark] text-white py-12 w-full ">
-          <div className="container mx-auto w-[343px] md:w-[704px] lg:w-[1240px]">
-            <div className="flex flex-col items-center space-y-8">
-              <div className="flex items-center space-x-4">
-                <Link href="/dashboard">
-                  <Image
-                    src="/logo-dark.svg"
-                    alt="CookBookie Logo"
-                    className="dark:invert"
-                    width={44}
-                    height={44}
-                    priority
+        <footer className="w-full">
+          <div className="bg-[--gray-dark] text-white py-12">
+            <div className="container mx-auto w-[343px] md:w-[704px] lg:w-[1240px]">
+              <div className="flex flex-col items-center space-y-8">
+                <div className="flex items-center space-x-4">
+                  <Link href="/dashboard">
+                    <Image
+                      src="/logo-dark.svg"
+                      alt="CookBookie Logo"
+                      className="dark:invert"
+                      width={44}
+                      height={44}
+                      priority
+                    />
+                  </Link>
+                  <h3 className="text-xl tracking-wider">CookBookie</h3>
+                </div>
+                <div>
+                  <ul className="flex flex-col items-center space-y-2">
+                    <li>
+                      <Link href="/dashboard/add-recipe">Add recipes</Link>
+                    </li>
+                    <li>
+                      <Link href="/dashboard/my-recipes">My recipes</Link>
+                    </li>
+                    <li>
+                      <Link href="/dashboard/favorites">Favorites</Link>
+                    </li>
+                    <li>
+                      <Link href="/dashboard/shopping-list">Shopping list</Link>
+                    </li>
+                  </ul>
+                </div>
+                <div className="space-y-2">
+                  <label className="sr-only ">Enter your email address</label>
+                  <Input
+                    id="newsletter"
+                    name="newsletter"
+                    type="email"
+                    placeholder="Enter your email address"
+                    required
                   />
-                </Link>
-                <h3 className="text-xl">CookBookie</h3>
-              </div>
-              <div>
-                <ul className="flex flex-col items-center space-y-2">
-                  <li>
-                    <Link href="/dashboard/add-recipe">Add recipes</Link>
-                  </li>
-                  <li>
-                    <Link href="/dashboard/my-recipes">My recipes</Link>
-                  </li>
-                  <li>
-                    <Link href="/dashboard/favorites">Favorites</Link>
-                  </li>
-                  <li>
-                    <Link href="/dashboard/shopping-list">Shopping list</Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="space-y-2">
-                <label className="sr-only ">Enter your email address</label>
-                <Input
-                  id="newsletter"
-                  name="newsletter"
-                  type="email"
-                  placeholder="Enter your email address"
-                  required
-                />
 
-                <Button className="w-60">Subscribe</Button>
+                  <Button type="submit" className="w-60">
+                    Subscribe
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="py-4 text-[--gray-dark]">
+            <div className="container mx-auto w-[343px] md:w-[704px] lg:w-[1240px]">
+              <div className="flex justify-center space-x-4 text-xs">
+                <span>© 2023 All Rights Reserved.</span>
+                <span>Terms of Service</span>
               </div>
             </div>
           </div>

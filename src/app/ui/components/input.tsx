efeb695ed4721b919991690dfd22b6ff411d65eb
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-18 12:47:38 +0200

    add icon for input component

--- src/app/ui/components/input.tsx
+++ src/app/ui/components/input.tsx
@@ -4,6 +4,7 @@ interface TextInputProps {
   type: "text" | "email" | "password";
   placeholder: string;
   required?: boolean;
+  iconID?: string;
 }
 
 const Input: React.FC<TextInputProps> = ({
@@ -11,17 +12,30 @@ const Input: React.FC<TextInputProps> = ({
   name,
   type,
   placeholder,
-  required = false,
+  required = true,
+  iconID,
 }) => {
   return (
-    <input
-      type={type}
-      id={id}
-      name={name}
-      className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-zinc-700"
-      placeholder={placeholder}
-      required={required}
-    />
+    <div className="relative">
+      <input
+        type={type}
+        name={name}
+        id={id}
+        className="w-full p-2 pl-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-zinc-700 transition duration-150 ease-in-out"
+        placeholder={placeholder}
+        required={required}
+      />
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+        <svg
+          className="h-5 w-5 text-gray-400"
+          stroke="#8c8c8c"
+          fill="transparent"
+          viewBox="0 0 20 20"
+        >
+          <use href={`/icons.svg#${iconID}`}></use>
+        </svg>
+      </div>
+    </div>
   );
 };
 

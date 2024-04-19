9487c46480c5268917b639626a4d938112eb37cc
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-19 18:54:42 +0200

    fix list element

--- src/app/ui/components/category-card.tsx
+++ src/app/ui/components/category-card.tsx
@@ -5,7 +5,9 @@ export default function CategoryCard() {
   return (
     <li>
       <h3 className="text-xl font-semibold mb-4">Title</h3>
-      <RecipeCard />
+      <ul>
+        <RecipeCard />
+      </ul>
       <Button variant="secondary" className="ml-auto">
         See all
       </Button>

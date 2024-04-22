4a05e208e27c5ba20f9eb0f93165816d1e1c4eed
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-22 14:08:21 +0200

    update shopping list page - template ui

--- src/app/dashboard/shopping-list/page.tsx
+++ src/app/dashboard/shopping-list/page.tsx
@@ -1,3 +1,20 @@
 export default function Page() {
-  return <p>Shopping list</p>;
+  return (
+    <div className="w-full flex flex-col">
+      <h2 className="text-xl font-semibold mb-8">Shopping list</h2>
+
+      <div className="flex justify-between space-x-2 mb-4 bg-[--primary-color] text-white p-2 rounded-lg">
+        <div className="w-1/2">Product</div>
+        <div className="w-1/4 flex justify-center">Quantity</div>
+        <div className="w-1/4 flex justify-center">Remove</div>
+      </div>
+      <ul className="px-2">
+        <li className="flex justify-between space-x-2 items-start pb-2 border-b border-gray-300">
+          <div className="w-1/2">Milk</div>
+          <div className="w-1/4 flex justify-center">1 liter</div>
+          <button className="w-1/4 flex justify-center">X</button>
+        </li>
+      </ul>
+    </div>
+  );
 }

bc39e2f7024673b5679965413ff6737fa962cc0a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-22 15:11:53 +0200

    add bg image to recipe card

--- src/app/ui/components/recipe-card-large.tsx
+++ src/app/ui/components/recipe-card-large.tsx
@@ -1,4 +1,22 @@
+import Image from "next/image";
+
+import { Button } from "./button";
+
 export default function RecipeCardLarge() {
-  return;
-  <div>Recipe Card</div>;
+  return (
+    <div>
+      <h2>Title</h2>
+      <p>Description</p>
+      <Button>Add to favorite recipes</Button>
+      <span>Time</span>
+      <Image
+        src="/pancakes.png"
+        layout="fill"
+        objectFit="contain"
+        alt="Description"
+        objectPosition="top"
+        className="t-0 l-0 z-[-1]"
+      />
+    </div>
+  );
 }

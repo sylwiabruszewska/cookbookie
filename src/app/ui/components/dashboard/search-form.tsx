d45cd4c845b88eb06bcad030a7a5f6380694a11a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-23 14:48:59 +0200

    import fontawesome icon

--- src/app/ui/components/dashboard/search-form.tsx
+++ src/app/ui/components/dashboard/search-form.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchForm: React.FC = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -22,23 +24,9 @@ const SearchForm: React.FC = () => {
         />
         <button
           type="submit"
-          className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full bg-[--gray-dark] hover:bg-[--primary-color] text-white rounded-r px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[--primary-color]"
+          className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full bg-[--gray-dark] hover:bg-[--primary-color] text-white rounded-r px-4 focus:outline-none focus:ring-2 focus:ring-[--primary-color] "
         >
-          <svg
-            className="w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
+          <FontAwesomeIcon icon={faSearch} className="text-sm w-full h-full" />
         </button>
       </div>
     </form>

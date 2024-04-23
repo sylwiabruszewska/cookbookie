f4643422638e31eaa08c6312e0fc0e678a722dd1
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-23 14:38:57 +0200

    create search page

---
+++ src/app/dashboard/search/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import SearchForm from "@/app/ui/components/dashboard/search-form";
+
+export default function Page() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-8">Search</h2>
+
+      <SearchForm />
+    </div>
+  );
+}

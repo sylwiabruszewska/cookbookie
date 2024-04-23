b73b71291dc16b9691a2501eeb6bc7a37ef42734
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-04-23 08:21:46 +0200

    check params for dynamic routes

--- src/app/dashboard/my-recipes/[recipeId]/page.tsx
+++ src/app/dashboard/my-recipes/[recipeId]/page.tsx
@@ -1,8 +1,9 @@
 import RecipeCardLarge from "@/app/ui/components/recipe-card-large";
 
-export default function Page() {
+export default function Page({ params }: { params: { recipeId: string } }) {
   return (
     <div>
+      <p>Recipe ID {params.recipeId}</p>
       <RecipeCardLarge />
     </div>
   );

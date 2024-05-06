5586ca6dd3f46f6e0a4edecc65493be7f49068d6
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-06 10:13:26 +0200

    change helpers function types to dispatch

--- src/app/utils/timePickerHelpers.tsx
+++ src/app/utils/timePickerHelpers.tsx
@@ -1,12 +1,18 @@
+import { Dispatch, SetStateAction } from "react";
+
+export type SetHoursFunction = Dispatch<SetStateAction<number>>;
+export type SetMinutesFunction = Dispatch<SetStateAction<number>>;
+export type SetFormattedTimeFunction = Dispatch<SetStateAction<string>>;
+
 export const handleIncrement = (
   minutes: number,
-  setHours: Function,
-  setMinutes: Function
+  setHours: SetHoursFunction,
+  setMinutes: SetMinutesFunction
 ) => {
   const newMinutes = minutes + 5;
   if (newMinutes >= 60) {
     setHours((prevHours: number) => prevHours + 1);
-    setMinutes((prevMinutes: number) => newMinutes % 60);
+    setMinutes(() => newMinutes % 60);
   } else {
     setMinutes((prevMinutes: number) => prevMinutes + 5);
   }
@@ -15,14 +21,14 @@ export const handleIncrement = (
 export const handleDecrement = (
   minutes: number,
   hours: number,
-  setHours: Function,
-  setMinutes: Function
+  setHours: SetHoursFunction,
+  setMinutes: SetMinutesFunction
 ) => {
   const newMinutes = minutes - 5;
   if (newMinutes < 0) {
     if (hours > 0) {
       setHours((prevHours: number) => prevHours - 1);
-      setMinutes((prevMinutes: number) => 60 + newMinutes);
+      setMinutes(() => 60 + newMinutes);
     }
   } else {
     setMinutes((prevMinutes: number) => prevMinutes - 5);
@@ -32,7 +38,7 @@ export const handleDecrement = (
 export const formatTime = (
   hours: number,
   minutes: number,
-  setFormattedTime: Function
+  setFormattedTime: SetFormattedTimeFunction
 ) => {
   const formattedHours = hours > 0 ? `${hours}h` : "";
   const formattedMinutes = minutes > 0 ? `${minutes}min` : "";

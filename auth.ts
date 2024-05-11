be48fd124a2aa121b3795e2e15ac9bf69dec6a6e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-11 17:07:09 +0200

    implement auth with credientals provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,43 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import { sql } from "@vercel/postgres";
+import type { User } from "@/models/User";
+import bcrypt from "bcrypt";
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const user = await getUser(email);
+
+        if (!user) {
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordsMatch) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
 });

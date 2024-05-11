b9adfd583f57e52333c7fa9aa17409f37395326f
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-11 17:07:27 +0200

    create authenticate action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { signIn } from "../../auth";
+import { AuthError } from "next-auth";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
 import { redirect } from "next/navigation";
@@ -25,4 +27,28 @@ export async function register(values: RegisterFormValues) {
     return "Database Error: Failed to Create Account.";
   }
   redirect("/login");
+}
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
+export async function authenticate(values: LoginFormValues) {
+  const { email, password } = values;
+
+  try {
+    await signIn("credentials", values);
+    return null;
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }

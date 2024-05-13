ddf1d8b36aaf4a965e4793e72b1cf688f3a88433
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-13 21:34:32 +0200

    fix input type password

--- src/ui/components/icon-input.tsx
+++ src/ui/components/icon-input.tsx
@@ -35,6 +35,7 @@ const IconInput: React.FC<TextInputProps> = ({
           className={`w-full p-2 pl-10 border border-[--gray] rounded-md focus:outline-none focus:ring-2 focus:ring-[--gray] transition duration-150 ease-in-out`}
           placeholder={placeholder}
           required={required}
+          type={type}
           {...field}
         />
         <div className="absolute inset-y-0 left-0 flex items-center pl-3">

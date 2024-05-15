b88771aedbb083ec7a1e3ba1f5e508be4bf83050
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-15 14:31:44 +0200

    update redirect in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,21 @@
-import NextAuth from "next-auth";
-import { authConfig } from "@/auth.config";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export default NextAuth(authConfig).auth;
+export async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isOnDashboard = path.startsWith("/dashboard");
+  const token = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!isOnDashboard && token) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+  }
+  if (isOnDashboard && !token) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|logo.svg|logo-dark.svg).*)"],

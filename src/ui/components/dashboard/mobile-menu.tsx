98dbf9902d1424770611a5a480ac13f0ab945748
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-16 12:23:11 +0200

    update mobile menu styles

--- src/ui/components/dashboard/mobile-menu.tsx
+++ src/ui/components/dashboard/mobile-menu.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 interface MobileMenuProps {
   closeMenu: () => void;
@@ -10,12 +13,33 @@ const MobileMenu = ({ closeMenu }: MobileMenuProps) => {
   };
 
   return (
-    <nav className="fixed inset-0 w-[100vw] h-[100vh] flex items-center justify-center bg-[var(--primary-color)] z-50">
-      <div className="absolute top-4 right-4">
-        <span onClick={closeMenu}>X</span>
+    <nav className="fixed inset-0 w-[100vw] h-[100vh] flex items-center justify-center bg-[#EBF3D4] z-50">
+      <div className="absolute top-4 left-6">
+        <Link href="/dashboard" className="flex items-center space-x-4">
+          <Image
+            src="/logo.svg"
+            alt="CookBookie Logo"
+            className="dark:invert"
+            width={44}
+            height={44}
+            priority
+          />
+          <p className="max-sm:hidden font-semibold text-lg text-black tracking-wide">
+            CookBookie
+          </p>
+        </Link>
+      </div>
+      <div className="absolute top-6 right-6">
+        <span onClick={closeMenu}>
+          <FontAwesomeIcon
+            icon={faXmark}
+            aria-label="Close"
+            className="w-[28px] h-[28px]"
+          />
+        </span>
       </div>
 
-      <ul className="flex flex-col items-center justify-center">
+      <ul className="flex flex-col items-center justify-center text-[18px] space-y-4 font-semibold">
         <li onClick={handleLinkClick}>
           <Link href="/dashboard/add-recipe">Add recipes</Link>
         </li>

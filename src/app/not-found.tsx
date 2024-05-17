6182e593b381717fbecc1892686f828d5fa1d07f
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-17 13:56:01 +0200

    update not found with layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,24 +2,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@ui/components/button";
+import HomepageLayout from "@ui/layouts/homepage";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center text-center h-screen">
-      <Image
-        src="/not-found.svg"
-        alt="Not found"
-        className="w-261 h-auto object-fit"
-        width={261}
-        height={170}
-        priority
-      />
-      <h1 className="text-lg font-bold">We are sorry,</h1>
-      <span>but the page you were looking for can’t be found.</span>
-      <Link href="/" className="mt-4">
-        <Button>Go back to home</Button>
-      </Link>
-    </div>
+    <HomepageLayout>
+      <div className="z-10 max-w-md w-[90vw] mx-auto p-6 bg-white rounded-lg shadow-md flex flex-col items-center">
+        <Image
+          src="/not-found.svg"
+          alt="Not found"
+          className="w-261 h-auto object-fit"
+          width={261}
+          height={170}
+          priority
+        />
+        <h1 className="text-lg font-bold">We are sorry,</h1>
+        <span>but the page you were looking for can’t be found.</span>
+        <Link href="/" className="mt-4">
+          <Button>Go back to home</Button>
+        </Link>
+      </div>
+    </HomepageLayout>
   );
 };
 

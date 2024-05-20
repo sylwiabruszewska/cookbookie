eda109e5a0f5c14aa9d2260cca07ddbc0b12efe8
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-20 21:37:09 +0200

    add private/public btn switch

---
+++ src/ui/components/add-recipe/switch.tsx
@@ -0,0 +1,38 @@
+import { Field, useFormikContext } from "formik";
+
+interface SwitchProps {
+  name: string;
+}
+
+export const Switch: React.FC<SwitchProps> = ({ name }) => {
+  const { setFieldValue, values } = useFormikContext<any>();
+  const isPublic = values[name];
+
+  const handleToggle = () => {
+    setFieldValue(name, !isPublic);
+  };
+
+  return (
+    <Field name={name}>
+      {() => (
+        <label className="cursor-pointer inline-flex items-center space-x-4 w-auto">
+          <button
+            type="button"
+            aria-pressed={isPublic}
+            onClick={handleToggle}
+            className={`relative w-12 h-6 duration-300 rounded-full cursor-pointerfocus:outline-none focus:ring-2 focus:ring-[--gray-dark] ${
+              isPublic ? "bg-[--gray-light]" : "bg-[--primary-color]"
+            }`}
+          >
+            <div
+              className={`absolute top-0.5 left-0.5 w-5 h-5 bg-white rounded-full shadow-md transform duration-300 ${
+                isPublic ? "translate-x-0" : "translate-x-6"
+              }`}
+            ></div>
+          </button>
+          <span>{isPublic ? "Public" : "Private"}</span>
+        </label>
+      )}
+    </Field>
+  );
+};

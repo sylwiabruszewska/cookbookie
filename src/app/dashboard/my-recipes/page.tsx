84fb71fff9b3a4acaba9e580b580f2724beebf00
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-22 22:50:28 +0200

    apply fetch user recipes

--- src/app/dashboard/my-recipes/page.tsx
+++ src/app/dashboard/my-recipes/page.tsx
@@ -1,13 +1,14 @@
-import { fetchRecipes } from "@lib/data";
+import { fetchUserRecipes } from "@lib/data";
 import { MyRecipes } from "@ui/components/recipes/my-recipes";
+import { getUserEmail } from "@utils/getUser";
 
-export default async function Page() {
-  const recipes = await fetchRecipes();
+export default async function MyRecipesPage() {
+  const userEmail = await getUserEmail();
+  const recipes = await fetchUserRecipes(userEmail);
 
   return (
     <div>
       <h2 className="text-xl font-semibold mb-8">My recipes</h2>
-
       {recipes ? <MyRecipes recipes={recipes} /> : <p>Add some recipes!</p>}
     </div>
   );

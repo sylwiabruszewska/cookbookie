b0f57dc226daf90b914043c47761819e922c6462
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-22 22:48:17 +0200

    remove use client from my recipes list

--- src/ui/components/recipes/my-recipes.tsx
+++ src/ui/components/recipes/my-recipes.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Recipe } from "@lib/definitions";
 import { RecipeCardMedium } from "@/ui/components/recipe-card-medium";
 
@@ -10,13 +8,13 @@ interface MyRecipesComponentProps {
 export const MyRecipes: React.FC<MyRecipesComponentProps> = ({ recipes }) => {
   return (
     <ul>
-      {recipes.map((recipe: any) => (
+      {recipes.map((recipe) => (
         <li key={recipe.id} className="mb-4">
           <RecipeCardMedium
             title={recipe.title}
             description={recipe.description}
             images={recipe.images}
-            cookingTime={recipe.cooking_time}
+            cookingTime={recipe.cookingTime}
           />
         </li>
       ))}

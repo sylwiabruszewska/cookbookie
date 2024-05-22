e4ca109df55dce6478a2961158b330a229c62e9f
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-22 22:49:46 +0200

    create fetch user recipes func

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,6 +3,7 @@ import { unstable_noStore as noStore } from "next/cache";
 
 import { Category, Recipe } from "./definitions";
 
+// ***** CATEGORIES *****
 export async function fetchCategories() {
   noStore();
 
@@ -15,6 +16,7 @@ export async function fetchCategories() {
   }
 }
 
+// ***** RECIPES *****
 export async function fetchRecipes() {
   noStore();
 
@@ -41,5 +43,25 @@ export async function getUserIdByEmail(email: string): Promise<string | null> {
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch user ID from database.");
+  }
+}
+
+// ***** USER RECIPES *****
+export async function fetchUserRecipes(userEmail: string): Promise<Recipe[]> {
+  noStore();
+
+  try {
+    const data = await sql<Recipe[]>`
+      SELECT recipes.*
+      FROM recipes
+      JOIN users ON recipes.owner_id = users.id
+      WHERE users.email = ${userEmail};
+    `;
+
+    const recipes: Recipe[] = data.rows.flat();
+    return recipes;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch user recipes.");
   }
 }

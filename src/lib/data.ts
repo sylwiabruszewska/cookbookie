c2ff7dc6d67efac4b40c619e06cbb4d465eec0bb
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-22 13:22:34 +0200

    add functions to fetch recipes (temp all), user id

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { unstable_noStore as noStore } from "next/cache";
 
-import { Category } from "./definitions";
+import { Category, Recipe } from "./definitions";
 
 export async function fetchCategories() {
   noStore();
@@ -12,5 +12,34 @@ export async function fetchCategories() {
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch the latest categories.");
+  }
+}
+
+export async function fetchRecipes() {
+  noStore();
+
+  try {
+    const data = await sql<Recipe>`SELECT * FROM recipes`;
+    return data.rows;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch the latest categories.");
+  }
+}
+
+export async function getUserIdByEmail(email: string): Promise<string | null> {
+  try {
+    const userResult = await sql`
+      SELECT id FROM users WHERE email = ${email}
+    `;
+
+    if (userResult.rowCount === 0) {
+      return null;
+    }
+
+    return userResult.rows[0].id;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch user ID from database.");
   }
 }

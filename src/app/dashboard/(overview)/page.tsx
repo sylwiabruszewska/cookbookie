a69da639b355de3bfb7d0d315f34623ff478f483
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-23 15:35:36 +0200

    implement fetching recent recipes on dashboard

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,12 +1,36 @@
 import HeroSection from "@/ui/components/dashboard/hero";
 import CategoryCard from "@/ui/components/category-card";
+import { fetchRecentRecipes, fetchCategories } from "@lib/data";
 
 export default async function Page() {
+  const categories = await fetchCategories();
+
+  const recentRecipesPromises = categories.map(async (category) => {
+    const recentRecipes = await fetchRecentRecipes(category.id);
+    return { categoryId: category.id, recipes: recentRecipes };
+  });
+
+  const recentRecipesForCategories = await Promise.all(recentRecipesPromises);
   return (
     <>
       <HeroSection />
       <ul className="list-none">
-        <CategoryCard />
+        {categories.map((category) => {
+          const matchingCategory = recentRecipesForCategories.filter(
+            (item) => item.categoryId === category.id
+          );
+          const recentRecipes =
+            matchingCategory.length > 0 ? matchingCategory[0].recipes : [];
+
+          return (
+            <li key={category.id} className="mb-8">
+              <CategoryCard
+                title={category.name}
+                recentRecipes={recentRecipes}
+              />
+            </li>
+          );
+        })}
       </ul>
     </>
   );

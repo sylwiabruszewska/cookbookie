b66e0584ccfbc746f1d78ac1ba89987aedaec0ef
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-23 15:34:20 +0200

    update recipe card, add props

--- src/ui/components/recipe-card-small.tsx
+++ src/ui/components/recipe-card-small.tsx
@@ -1,20 +1,25 @@
+import { Recipe } from "@lib/definitions";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function RecipeCardSmall() {
+interface RecipeCardSmallProps {
+  recipe: Recipe;
+}
+
+export default function RecipeCardSmall({ recipe }: RecipeCardSmallProps) {
   return (
     <li className="mb-8">
       <Link href="/">
-        <div className="relative h-323 rounded-lg">
+        <div className="relative w-[343px] h-[343px] rounded-lg overflow-hidden">
           <Image
-            src="/pancakes.png"
-            width={343}
-            height={323}
+            src={recipe.images[0]}
+            fill
             className="object-cover"
-            alt="Description"
+            alt={recipe.title}
+            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, (max-width: 1280px) 33vw, 25vw"
           />
           <div className="absolute bottom-0 left-0 right-0 m-4 bg-white rounded-lg p-4">
-            Description
+            {recipe.title}
           </div>
         </div>
       </Link>

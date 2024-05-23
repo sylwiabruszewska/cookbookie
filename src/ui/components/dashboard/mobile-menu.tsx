c948b423aec2563625ee7c27fcefe8fbcc325547
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-23 16:45:35 +0200

    update mobile menu styles

--- src/ui/components/dashboard/mobile-menu.tsx
+++ src/ui/components/dashboard/mobile-menu.tsx
@@ -24,58 +24,64 @@ const MobileMenu = ({ closeMenu }: MobileMenuProps) => {
   }, []);
 
   return (
-    <nav className="fixed inset-0 w-[100vw] h-[100vh] flex items-center justify-center bg-[--green-light] z-50">
-      <div className="absolute top-4 left-6">
-        <Link href="/dashboard" className="flex items-center space-x-4">
-          <Image
-            src="/logo.svg"
-            alt="CookBookie Logo"
-            className="dark:invert"
-            width={44}
-            height={44}
-            priority
-          />
-          <p className="max-sm:hidden font-semibold text-lg text-black tracking-wide">
-            CookBookie
-          </p>
-        </Link>
-      </div>
-      <div className="absolute top-6 right-6">
-        <span onClick={closeMenu}>
-          <FontAwesomeIcon
-            icon={faXmark}
-            aria-label="Close"
-            className="w-[28px] h-[28px]"
-          />
-        </span>
-      </div>
-
-      <ul className="flex flex-col items-center justify-center text-[18px] space-y-4 font-semibold">
-        <li onClick={handleLinkClick}>
-          <Link href="/dashboard/add-recipe">Add recipes</Link>
-        </li>
-        <li onClick={handleLinkClick}>
-          <Link href="/dashboard/my-recipes">My recipes</Link>
-        </li>
-        <li onClick={handleLinkClick}>
-          <Link href="/dashboard/favorites">Favorites</Link>
-        </li>
-        <li onClick={handleLinkClick}>
-          <Link href="/dashboard/shopping-list">Shopping list</Link>
-        </li>
-        <li onClick={handleLinkClick}>
+    <nav className="fixed inset-0 w-[100vw] h-[100vh] bg-[--green-light] z-50">
+      <div className="relative content-container h-full mx-auto flex items-center justify-center">
+        <div className="absolute top-4 left-0">
           <Link
-            href="/dashboard/search"
-            className="flex items-center space-x-2"
+            href="/dashboard"
+            onClick={closeMenu}
+            className="flex items-center space-x-4"
           >
-            <FontAwesomeIcon
-              icon={faSearch}
-              className="text-sm w-[20px] h-[20px]"
+            <Image
+              src="/logo.svg"
+              alt="CookBookie Logo"
+              className="dark:invert"
+              width={44}
+              height={44}
+              priority
             />
-            <span>Search</span>
+            <p className="max-md:hidden heading-brand font-semibold text-black">
+              CookBookie
+            </p>
           </Link>
-        </li>
-      </ul>
+        </div>
+        <div className="absolute top-6 right-0">
+          <button onClick={closeMenu}>
+            <FontAwesomeIcon
+              icon={faXmark}
+              aria-label="Close"
+              className="w-[28px] h-[28px]"
+            />
+          </button>
+        </div>
+
+        <ul className="flex flex-col items-center justify-center text-[18px] space-y-4 font-semibold">
+          <li onClick={handleLinkClick}>
+            <Link href="/dashboard/add-recipe">Add recipes</Link>
+          </li>
+          <li onClick={handleLinkClick}>
+            <Link href="/dashboard/my-recipes">My recipes</Link>
+          </li>
+          <li onClick={handleLinkClick}>
+            <Link href="/dashboard/favorites">Favorites</Link>
+          </li>
+          <li onClick={handleLinkClick}>
+            <Link href="/dashboard/shopping-list">Shopping list</Link>
+          </li>
+          <li onClick={handleLinkClick}>
+            <Link
+              href="/dashboard/search"
+              className="flex items-center space-x-2"
+            >
+              <FontAwesomeIcon
+                icon={faSearch}
+                className="text-sm w-[20px] h-[20px]"
+              />
+              <span>Search</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };

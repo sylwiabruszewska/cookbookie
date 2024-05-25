a063808695f21b759e3353f214d77a2eabc629d6
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-25 13:56:07 +0200

    remove unnecessary file

--- src/ui/components/recipes/my-recipes.tsx
+++ src/ui/components/recipes/my-recipes.tsx
@@ -1,4 +1,4 @@
-import { Recipe } from "@lib/definitions";
+import { Recipe } from "@/lib/definitions";
 import { RecipeCardMedium } from "@/ui/components/my-recipes/recipe-card-medium";
 
 interface MyRecipesComponentProps {
@@ -11,6 +11,7 @@ export const MyRecipes: React.FC<MyRecipesComponentProps> = ({ recipes }) => {
       {recipes.map((recipe) => (
         <li key={recipe.id} className="mb-4">
           <RecipeCardMedium
+            id={recipe.id}
             title={recipe.title}
             description={recipe.description}
             images={recipe.images}

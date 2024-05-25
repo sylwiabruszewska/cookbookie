019e6e1fdc1370d7db1302a01d63f97fc5aeb7ad
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-25 13:44:23 +0200

    aa actions to add or remove recipe from favorites, revalidate path favorites

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,12 +1,45 @@
-import { sql } from "@vercel/postgres";
-import { User } from "@/lib/definitions";
+"use server";
 
-export async function getUser(email: string): Promise<User | undefined> {
+import { sql } from "@vercel/postgres";
+import { unstable_noStore as noStore } from "next/cache";
+import { revalidatePath } from "next/cache";
+
+import { getUserEmail } from "@utils/getUser";
+import { getUserIdByEmail } from "@/lib/data";
+
+// ***** ADD RECIPE TO FAVORITES *****
+export async function addToFavorites(recipeId: string) {
+  noStore();
+
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0] as User | undefined;
+    const userEmail = await getUserEmail();
+    const userId = await getUserIdByEmail(userEmail);
+
+    await sql`INSERT INTO UserFavorites (userId, recipeId) VALUES (${userId}, ${recipeId})
+    `;
+
+    console.log("Recipe added to favorites");
+    revalidatePath("/dashboard/favorites");
   } catch (error) {
-    console.error("Failed to fetch user:", error);
-    throw new Error("Failed to fetch user.");
+    console.error("Database Error:", error);
+    throw new Error("Failed to add recipe to favorites.");
+  }
+}
+
+// ***** REMOVE RECIPE FROM FAVORITES *****
+export async function removeFromFavorites(recipeId: string) {
+  noStore();
+
+  try {
+    const userEmail = await getUserEmail();
+    const userId = await getUserIdByEmail(userEmail);
+
+    await sql`DELETE FROM UserFavorites WHERE userId = ${userId} AND recipeId = ${recipeId}`;
+
+    console.log("Recipe removed from favorites");
+    revalidatePath("/dashboard/favorites");
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to remove recipe from favorites.");
   }
 }

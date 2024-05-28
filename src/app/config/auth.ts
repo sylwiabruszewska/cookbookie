766fb23e57a4fd412e26fd79bcd630109fba3ba5
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-28 14:15:24 +0200

    create auth helper func to avoid passing config opts within getServerSession

---
+++ src/app/config/auth.ts
@@ -0,0 +1,21 @@
+import type {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import type { NextAuthOptions } from "next-auth";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@app/config/auth-config";
+
+export const config = {
+  ...authOptions,
+} satisfies NextAuthOptions;
+
+export function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, config);
+}

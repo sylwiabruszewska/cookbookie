37f5d80b6cce1d26469145bd04d23e6f9f299b48
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-28 17:01:37 +0200

    replace add recipe api route with server action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,16 +4,50 @@ import { sql } from "@vercel/postgres";
 import { unstable_noStore as noStore } from "next/cache";
 import { revalidatePath } from "next/cache";
 
-import { getUserEmail } from "@utils/getUser";
-import { getUserIdByEmail } from "@/lib/data";
+import { getUserId } from "@utils/getUser";
+import { Recipe } from "@/lib/definitions";
+
+// ***** ADD RECIPE *****
+export async function addRecipe(formData: Recipe) {
+  const {
+    images,
+    title,
+    description,
+    category_id,
+    cooking_time,
+    ingredients,
+    steps,
+    is_public,
+  } = formData;
+
+  try {
+    const userId = await getUserId();
+
+    const result = await sql`
+    INSERT INTO recipes (
+      images, title, description, category_id, cooking_time, ingredients, steps, is_public, owner_id
+    ) VALUES (
+      ${JSON.stringify(
+        images
+      )}, ${title}, ${description}, ${category_id}, ${cooking_time}, ${JSON.stringify(
+      ingredients
+    )}, ${JSON.stringify(steps)}, ${is_public}, ${userId}
+    ) RETURNING *`;
+
+    console.log("Recipe added successfully");
+    return result;
+  } catch (error) {
+    console.error("Failed to add recipe:", error);
+    throw new Error("Failed to add recipe.");
+  }
+}
 
 // ***** ADD RECIPE TO FAVORITES *****
 export async function addToFavorites(recipeId: string) {
   noStore();
 
   try {
-    const userEmail = await getUserEmail();
-    const userId = await getUserIdByEmail(userEmail);
+    const userId = await getUserId();
 
     await sql`INSERT INTO UserFavorites (userId, recipeId) VALUES (${userId}, ${recipeId})
     `;
@@ -31,8 +65,7 @@ export async function removeFromFavorites(recipeId: string) {
   noStore();
 
   try {
-    const userEmail = await getUserEmail();
-    const userId = await getUserIdByEmail(userEmail);
+    const userId = await getUserId();
 
     await sql`DELETE FROM UserFavorites WHERE userId = ${userId} AND recipeId = ${recipeId}`;
 

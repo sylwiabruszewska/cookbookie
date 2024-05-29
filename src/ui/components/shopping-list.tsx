ad79b55c43505c5c8b77112e72ff14196f83382e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-29 22:32:16 +0200

    update shoppinglist component

--- src/ui/components/shopping-list.tsx
+++ src/ui/components/shopping-list.tsx
@@ -4,8 +4,13 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { Button } from "@/ui/components/button";
+import { Ingredient } from "@lib/definitions";
 
-const ShoppingList = () => {
+interface ShoppingListProps {
+  userShoppingList: Ingredient[];
+}
+
+const ShoppingList: React.FC<ShoppingListProps> = ({ userShoppingList }) => {
   return (
     <>
       <div className="flex justify-between space-x-2 mb-4 bg-[--primary-color] text-white p-2 rounded-lg">
@@ -14,19 +19,24 @@ const ShoppingList = () => {
         <div className="w-1/4 flex justify-center">Remove</div>
       </div>
       <ul className="px-2">
-        <li className="flex justify-between space-x-2 items-center pb-2 border-b border-gray-300">
-          <div className="w-1/2">Milk</div>
-          <div className="w-1/4 flex justify-center">1 liter</div>
-          <div className="w-1/4 flex justify-center">
-            <Button variant="icon">
-              <FontAwesomeIcon
-                icon={faXmark}
-                aria-label="Remove"
-                className="h-4 w-4"
-              />
-            </Button>
-          </div>
-        </li>
+        {userShoppingList.map((ingredient: Ingredient) => (
+          <li
+            key={ingredient.id}
+            className="flex justify-between space-x-2 items-center pb-2 border-b border-gray-300"
+          >
+            <div className="w-1/2">{ingredient.ingredient}</div>
+            <div className="w-1/4 flex justify-center">{`${ingredient.quantity} ${ingredient.quantityUnit}`}</div>
+            <div className="w-1/4 flex justify-center">
+              <Button variant="icon">
+                <FontAwesomeIcon
+                  icon={faXmark}
+                  aria-label="Remove"
+                  className="h-4 w-4"
+                />
+              </Button>
+            </div>
+          </li>
+        ))}
       </ul>
     </>
   );

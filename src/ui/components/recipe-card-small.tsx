9d32def8d43d90702d64794c07ce82d81acccd7d
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-05-30 12:31:37 +0200

    update recipa small card - change width, height

--- src/ui/components/recipe-card-small.tsx
+++ src/ui/components/recipe-card-small.tsx
@@ -8,21 +8,19 @@ interface RecipeCardSmallProps {
 
 export default function RecipeCardSmall({ recipe }: RecipeCardSmallProps) {
   return (
-    <li className="mb-8">
-      <Link href={`/dashboard/recipes/${recipe.id}`}>
-        <div className="relative w-[343px] h-[343px] rounded-lg overflow-hidden">
-          <Image
-            src={recipe.images[0]}
-            fill
-            className="object-cover"
-            alt={recipe.title}
-            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, (max-width: 1280px) 33vw, 25vw"
-          />
-          <div className="absolute bottom-0 left-0 right-0 m-4 bg-white rounded-lg p-4">
-            {recipe.title}
-          </div>
+    <Link href={`/dashboard/recipes/${recipe.id}`}>
+      <div className="relative w-full h-[340px] rounded-lg overflow-hidden">
+        <Image
+          src={recipe.images[0]}
+          fill
+          className="object-cover"
+          alt={recipe.title}
+          sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, (max-width: 1280px) 33vw, 25vw"
+        />
+        <div className="absolute bottom-0 left-0 right-0 m-4 bg-white rounded-lg p-4">
+          {recipe.title}
         </div>
-      </Link>
-    </li>
+      </div>
+    </Link>
   );
 }

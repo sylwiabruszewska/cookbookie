66d0c1fde59ed200598ac816caabf5d58a0eb506
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-01 13:22:08 +0200

    update btn styles

--- src/ui/components/button.tsx
+++ src/ui/components/button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   type?: "submit" | "reset" | "button";
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
-  variant?: "green" | "crazyRounded" | "dark" | "border" | "icon";
+  variant?: "green" | "crazyRounded" | "dark" | "border" | "icon" | "logout";
   className?: string;
 }
 
@@ -15,29 +15,29 @@ export function Button({
   variant = "green",
   className,
 }: ButtonProps) {
+  const baseClass =
+    "flex items-center justify-center text-sm font-medium h-10 transition-colors duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2";
+
   const variantClasses = clsx({
-    "bg-[--primary-color] text-white hover:bg-[--gray-dark] hover:text-white px-2 h-10":
+    "bg-[--primary-color] text-white hover:bg-[--gray-dark] hover:text-white rounded-lg px-2":
       variant === "green",
-    "bg-[--primary-color] text-white hover:bg-[--gray-dark] hover:text-white px-5 rounded-tl-[15px] rounded-bl-[40px] rounded-br-[15px] rounded-tr-[40px] h-10":
+    "bg-[--primary-color] text-white hover:bg-[--gray-dark] hover:text-white btn-rounded px-5":
       variant === "crazyRounded",
-    "bg-[--gray-dark] text-white border-[--primary-color] hover:bg-[--primary-color] hover:border-white px-2 h-10":
+    "bg-[--gray-dark] text-white hover:bg-[--primary-color] rounded-lg px-2 h-10":
       variant === "dark",
-    "bg-[--primary-color] text-white border-2 border-[--primary-color] hover:bg-transparent hover:border-white px-2 h-10":
+    "bg-[--primary-color] text-white border-2 border-[--primary-color] hover:bg-transparent hover:border-white rounded-lg px-2":
       variant === "border",
-    "bg-transparent text-[--gray-medium] hover:text-[black] h-10 w-10":
+    "bg-transparent text-[--gray-medium] hover:text-[black] w-10":
       variant === "icon",
+    "bg-[--gray-dark] text-white border-2 border-white btn-rounded px-3":
+      variant === "logout",
   });
 
   return (
     <button
       type={type}
       onClick={onClick}
-      className={clsx(
-        className,
-        "flex items-center justify-center rounded-lg text-sm font-medium transition-colors duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
-        variantClasses,
-        className
-      )}
+      className={clsx(baseClass, variantClasses, className)}
     >
       {children}
     </button>

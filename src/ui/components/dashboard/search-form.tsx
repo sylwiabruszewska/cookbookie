3197d3c77592a78ad836b997179c3edba686d23b
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-02 19:19:22 +0200

    implement use search params in search form

--- src/ui/components/dashboard/search-form.tsx
+++ src/ui/components/dashboard/search-form.tsx
@@ -1,13 +1,30 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
 import { Button } from "@/ui/components/button";
 
-// import Icon from "../icon";
+const SearchForm = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-const SearchForm: React.FC = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
+
+  function handleSearch(query: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (query) {
+      params.set("query", query);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
 
   return (
     <form onSubmit={handleSubmit} className="w-[300px] lg:w-[400px] ">
@@ -20,6 +37,10 @@ const SearchForm: React.FC = () => {
           id="search"
           placeholder="Search recipes"
           className="w-full border border-[--gray] focus:outline-none focus:ring-2 focus:ring-[--gray-dark] focus:border-[--gray-dark] rounded-tl-[20px] rounded-bl-[40px] rounded-br-[20px] rounded-tr-[40px] h-10 p-6 lg:p-8 pr-24"
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+          defaultValue={searchParams.get("query")?.toString()}
         />
         <Button
           variant="dark"

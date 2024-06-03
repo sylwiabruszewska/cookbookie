d968b324025c0b3c43a0f59fde8c622c2792affb
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-03 16:52:24 +0200

    fetch categories on categories page

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,7 +1,20 @@
+import { fetchCategories } from "@lib/data";
+import { Button } from "@ui/components/button";
+
 export default async function Page() {
+  const categories = await fetchCategories();
+
   return (
     <div>
       <h2 className="heading-l">Categories</h2>
+
+      <ul className="flex flex-wrap gap-4">
+        {categories.map((category) => (
+          <li key={category.id} className="mb-4">
+            <Button>{category.name}</Button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

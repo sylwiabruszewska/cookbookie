09409b5fd0544d30ea88d439074d6d65f72cdc13
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-03 15:09:59 +0200

    add hover - recipe card small

--- src/ui/components/recipe-card-small.tsx
+++ src/ui/components/recipe-card-small.tsx
@@ -8,17 +8,17 @@ interface RecipeCardSmallProps {
 
 export default function RecipeCardSmall({ recipe }: RecipeCardSmallProps) {
   return (
-    <Link href={`/dashboard/recipes/${recipe.id}`}>
+    <Link href={`/dashboard/recipes/${recipe.id}`} className="group">
       <div className="relative w-full h-[340px] rounded-lg overflow-hidden">
         <Image
           src={recipe.images[0]}
           fill
-          className="object-cover"
+          className="object-cover transform duration-500 transition-transform group-hover:scale-105"
           alt={recipe.title}
           sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, (max-width: 1280px) 33vw, 25vw"
         />
-        <div className="absolute bottom-0 left-0 right-0 m-4 bg-white rounded-lg p-4">
-          {recipe.title}
+        <div className="absolute bottom-0 left-0 right-0 m-4 bg-white rounded-lg p-4 shadow-sm">
+          <h3 className="relative z-50">{recipe.title}</h3>
         </div>
       </div>
     </Link>

ed06ccd55fd0b7a6bf81be3351a52a5fabdba830
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-04 20:51:44 +0200

    add shimmer animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,11 @@ const config: Config = {
           "50%": { opacity: "1", transform: "translate(-50%, -50%)" },
           "100%": { opacity: "0", transform: "translate(-50%, -200%)" },
         },
+        shimmer: {
+          "100%": {
+            transform: "translateX(100%)",
+          },
+        },
       },
     },
     fontSize: {

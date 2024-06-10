e403c41c9ca33c8867f03c969ea17eb9bfc2ef6e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-10 12:55:14 +0200

    create theme provider

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/ui/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {

d849c8f18947e7e8e735516cf21ff5cd7c1b6474
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-17 11:44:41 +0200

    pass down ingredients prop

--- src/app/[locale]/dashboard/recipes/[recipeId]/edit/page.tsx
+++ src/app/[locale]/dashboard/recipes/[recipeId]/edit/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 import { Suspense } from "react";
 import initTranslations from "@utils/i18n";
 
-import { fetchCategories, fetchRecipeById } from "@lib/data";
+import { fetchCategories, fetchIngredients, fetchRecipeById } from "@lib/data";
 import EditForm from "@ui/components/recipes/edit-form";
 import { Loader } from "@ui/components/loader";
 import { getLocale } from "@lib/getLocal";
@@ -23,6 +23,7 @@ export default async function Page({
   ]);
 
   const categories = translateCategories(categoriesData, t);
+  const ingredients = await fetchIngredients();
 
   if (!recipe) {
     notFound();
@@ -32,7 +33,11 @@ export default async function Page({
     <div>
       <h2 className="heading-l">{t("edit_recipe")}</h2>
       <Suspense fallback={<Loader />}>
-        <EditForm recipe={recipe} categories={categories} />
+        <EditForm
+          recipe={recipe}
+          categories={categories}
+          ingredientsFromDb={ingredients}
+        />
       </Suspense>
     </div>
   );

c1506acad5fa28b739b2ef26628c370ba94379de
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-18 13:13:28 +0200

    render translated message when nothing on shoppinglist

--- src/ui/components/shopping-list/shopping-list-content.tsx
+++ src/ui/components/shopping-list/shopping-list-content.tsx
@@ -3,12 +3,23 @@ import { Suspense } from "react";
 import { fetchUserShoppingList } from "@lib/data";
 import { ShoppingList } from "@ui/components/shopping-list/shopping-list";
 import { Loader } from "@ui/components/loader";
+import initTranslations from "@utils/i18n";
+import { getLocale } from "@lib/getLocal";
 
 export async function ShoppingListContent() {
+  const locale = getLocale();
+  const { t } = await initTranslations(locale, ["dashboard"]);
+
   const userShoppingList = await fetchUserShoppingList();
   return (
-    <Suspense fallback={<Loader />}>
-      <ShoppingList userShoppingList={userShoppingList} />
-    </Suspense>
+    <>
+      {userShoppingList.length > 0 ? (
+        <Suspense fallback={<Loader />}>
+          <ShoppingList userShoppingList={userShoppingList} />
+        </Suspense>
+      ) : (
+        <p>{t("no_shopping_list")}</p>
+      )}
+    </>
   );
 }

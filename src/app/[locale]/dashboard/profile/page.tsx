52af6974625042274c50e9f8e88cfe599ee579f1
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-19 16:24:49 +0200

    remove suspense - not working

--- src/app/[locale]/dashboard/profile/page.tsx
+++ src/app/[locale]/dashboard/profile/page.tsx
@@ -1,7 +1,4 @@
-import { Suspense } from "react";
-
 import initTranslations from "@utils/i18n";
-import { Loader } from "@ui/components/loader";
 import { getLocale } from "@lib/getLocal";
 import UserProfile from "@ui/components/profile/user-profile";
 
@@ -12,10 +9,7 @@ export default async function Page() {
   return (
     <div>
       <h2 className="heading-l">{t("profile")}</h2>
-
-      <Suspense fallback={<Loader />}>
-        <UserProfile />
-      </Suspense>
+      <UserProfile />
     </div>
   );
 }

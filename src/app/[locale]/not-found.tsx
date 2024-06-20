a9265a053058156e61b133cb00467fdb9fbb85bd
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-20 19:20:44 +0200

    fix displaying custom 404 page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -11,22 +11,21 @@ export default async function NotFound() {
   const { t } = await initTranslations(locale, ["dashboard"]);
 
   return (
-    <HomepageLayout>
-      <div className="z-10 max-w-md w-[90vw] mx-auto p-6 bg-white rounded-lg shadow-md flex flex-col items-center">
-        <Image
-          src="/not-found.svg"
-          alt="Not found"
-          className="w-261 h-auto object-fit"
-          width={261}
-          height={170}
-          priority
-        />
-        <h1 className="text-lg font-bold">{t("not_found_h")}</h1>
-        <span>{t("not_found_t")}</span>
-        <Link href="/" className="mt-4">
-          <Button className="btn-green">{t("not_found_b")}</Button>
-        </Link>
-      </div>
-    </HomepageLayout>
+    <div className="bg-white dark:bg-[--gray-medium] flex flex-col items-center justify-center h-screen">
+      <Image
+        src="/not-found.svg"
+        alt="Not found"
+        className="w-261 h-auto object-fit"
+        width={261}
+        height={170}
+        priority
+      />
+      <h1 className="text-[50px]">404</h1>
+      <h2 className="text-lg font-bold">{t("not_found_h")}</h2>
+      <span>{t("not_found_t")}</span>
+      <Link href="/" className="mt-4">
+        <Button className="btn-green">{t("not_found_b")}</Button>
+      </Link>
+    </div>
   );
 }

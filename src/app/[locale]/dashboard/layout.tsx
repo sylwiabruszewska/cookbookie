94e7f6e0180aa414fb3e2d646deb73fdb25b1409
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-22 20:02:27 +0200

    add dashboard layout

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import NavBar from "@ui/components/dashboard/navbar";
-import Footer from "@ui/components/dashboard/footer";
+import DashboardLayout from "@ui/layouts/dashboard";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <NavBar />
-
-      <main className="flex-grow overflow-y-auto w-full mb-8">
-        <div className="content-container">{children}</div>
-      </main>
-
-      <Footer />
-    </div>
-  );
+  return <DashboardLayout>{children}</DashboardLayout>;
 }
---
+++ src/ui/layouts/dashboard.tsx
@@ -0,0 +1,20 @@
+import { NavBar } from "@ui/components/dashboard/navbar";
+import { Footer } from "@ui/components/dashboard/footer";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen ">
+      <NavBar />
+
+      <main className="flex-grow overflow-y-auto w-full mb-8">
+        <div className="content-container">{children}</div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}

7596ab551f858bec0b87dd645058094847a049ed
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-22 12:35:25 +0200

    add translation for error page

--- src/app/[locale]/dashboard/error.tsx
+++ src/app/[locale]/dashboard/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 import { Button } from "@ui/components/button";
 
@@ -11,15 +12,17 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const { t } = useTranslation(["dashboard"]);
+
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
     <div className="flex h-[50vh] flex-col items-center justify-center">
-      <h2 className="text-center">Something went wrong!</h2>
+      <h2 className="text-center">{t("sth_went_wrong")}</h2>
       <Button className="mt-4 btn-green" onClick={() => reset()}>
-        Try again
+        {t("try_again")}
       </Button>
     </div>
   );

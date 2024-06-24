d5dc37117e3a88d7c3c31f84a182b5d826e6717d
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-24 13:56:15 +0200

    update iconinput - remove meta errors, tidy

--- src/ui/components/common/icon-input.tsx
+++ src/ui/components/common/icon-input.tsx
@@ -4,11 +4,12 @@ import { useField } from "formik";
 import { useTranslation } from "react-i18next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const baseClass =
+  "w-full h-10 p-2 pl-10 border border-[--gray] rounded-md focus:outline-none focus:ring-2 focus:ring-[--gray] transition duration-150 ease-in-out bg-[--background] dark:bg-transparent dark:text-[--font] dark:placeholder-[--gray]";
+
 interface TextInputProps {
-  id: string;
   name: string;
   type: "text" | "email" | "password";
-  required?: boolean;
   iconID?: string;
   label: string;
   autocomplete?: "on" | "off";
@@ -16,13 +17,11 @@ interface TextInputProps {
 }
 
 export const IconInput: React.FC<TextInputProps> = ({
-  id,
   name,
   type,
-  required = true,
   iconID,
   label,
-  autocomplete = "on",
+  autocomplete = "off",
   className,
 }) => {
   const { t } = useTranslation(["dashboard"]);
@@ -42,18 +41,17 @@ export const IconInput: React.FC<TextInputProps> = ({
   };
 
   return (
-    <label
-      className={clsx("relative mb-6 flex items-center w-full", className)}
-    >
+    <label className={clsx("w-full", className)}>
       <span className="sr-only">{label}</span>
       <div className="relative w-full">
         <input
-          id={id}
-          className={`w-full p-2 pl-10 border border-[--gray] rounded-md focus:outline-none focus:ring-2 focus:ring-[--gray] transition duration-150 ease-in-out bg-[--background] dark:bg-transparent dark:text-[--font] dark:placeholder-[--gray]`}
+          id={name}
+          {...field}
+          className={baseClass}
           placeholder={label}
           type={inputType}
           autoComplete={autocomplete}
-          {...field}
+          aria-describedby={`${name}-error`}
         />
         <div className="absolute inset-y-0 left-0 flex items-center pl-3">
           <svg
@@ -86,11 +84,6 @@ export const IconInput: React.FC<TextInputProps> = ({
             )}
           </div>
         )}
-        {meta.touched && meta.error ? (
-          <div className="absolute t-10 l-0 text-xs text-[#E74A3B]">
-            {meta.error}
-          </div>
-        ) : null}
       </div>
     </label>
   );

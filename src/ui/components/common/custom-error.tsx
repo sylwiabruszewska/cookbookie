301a0e64c4ac9eb78cefe78110abdab86bba6c86
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-24 13:56:37 +0200

    create custom error message component

---
+++ src/ui/components/common/custom-error.tsx
@@ -0,0 +1,24 @@
+import { ErrorMessage } from "formik";
+
+interface CustomErrorProps {
+  name: string;
+  className?: string;
+}
+
+export const CustomErrorMessage: React.FC<CustomErrorProps> = ({
+  name,
+  className,
+}) => {
+  const errorId = `${name}-error`;
+
+  return (
+    <div
+      className={className}
+      id={errorId}
+      aria-live="polite"
+      aria-atomic="true"
+    >
+      <ErrorMessage name={name} component="div" className="error-text w-full" />
+    </div>
+  );
+};

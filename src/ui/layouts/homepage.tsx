74a189f67fa9113c5e993684be123617cfd1f714
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-06-24 18:40:23 +0200

    change home bg

--- src/ui/layouts/homepage.tsx
+++ src/ui/layouts/homepage.tsx
@@ -6,8 +6,8 @@ export function HomepageLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="relative flex min-h-screen min-w-screen flex-col justify-center main-background bg-center bg-cover">
-      <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+    <main className="relative flex min-h-screen min-w-screen flex-col justify-center main-background bg-left lg:bg-center bg-cover">
+      <div className="absolute inset-0 bg-black opacity-70 z-0"></div>
       {children}
 
       <div className="absolute top-4 left-4">
